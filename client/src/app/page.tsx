19b1a3755715f3a002efcfdbab0a64fd81eef163
Hasintha Sonuka <Hasintha_S@epic.lk>
2023-06-02 21:41:42 +0530

    FE init commmit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Fragment, useState } from 'react'
+import Header from './components/header';
+import Home from './pages/home/home';
+import foodcity from './pages/foodcity-section/foodcity';
+import bite from './pages/bite-section/bite';
+
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+export default function Page() {
+  return (
+    <>
+      <Home></Home>
+    </>
+  )
+}
---
+++ client/src/app/pages/foodcity-section/foodcity.tsx
@@ -0,0 +1,10 @@
+import Header from '@/app/components/header'
+import React from 'react'
+
+export default function foodcity() {
+  return (
+    <div>
+      <Header></Header>
+      foodcity</div>
+  )
+}
